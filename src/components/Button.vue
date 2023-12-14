fa17138e76eded700949f6287da3e23b2f0eeca9
PureJoyMind <mazdak.parniandg@gmail.com>
2023-12-15 00:37:52 +0330

    Created task tracker and added delete and toggle

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="addTask()" :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props:{
+        text: String,
+        color: String
+    },
+    methods:{  
+        addTask: function () {
+            console.log('button');
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button  color="green" text="Add Task" />
+        <!-- <Button color="blue" text="Update Task" />
+        <Button color="red" text="Delete Task" /> -->
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+    name: 'Header',
+    props:{
+        title: {
+            type: String,
+            default: 'default value of title'
+        }
+    },
+    components:    {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
